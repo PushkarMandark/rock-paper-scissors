784d2a074ab6c7d9559796d8b0d138b5d04f25c8
Pushkar kumar <pushkar.meclpu@gmail.com>
2022-10-04 15:08:20 +0530

    adding to ui team

--- test.js
+++ test.js
@@ -1,3 +1,28 @@
+let round = 0;
+let humanScore = 0;
+let compScore = 0;
+
+const result_div = document.querySelector(".show_result");
+const round_div = document.querySelector(".show_round");
+const humanScoreDiv = document.querySelector(".show_score_human");
+const compScoreDiv = document.querySelector(".show_score_computer");
+
+humanScoreDiv.textContent = humanScore;
+compScoreDiv.textContent = compScore;
+round_div.textContent = round;
+
+document.querySelector(".rock").addEventListener("click", () => {
+  result_div.textContent = playRound("rock", getComputerChoice());
+});
+
+document.querySelector(".paper").addEventListener("click", () => {
+  result_div.textContent = playRound("paper", getComputerChoice());
+});
+
+document.querySelector(".scissor").addEventListener("click", () => {
+  result_div.textContent = playRound("scissors", getComputerChoice());
+});
+
 function getComputerChoice() {
   const randNum = Math.floor(Math.random() * 3);
   switch (randNum) {
@@ -16,32 +41,73 @@ function getComputerChoice() {
 }
 
 function playRound(playerSelection, computerSelection) {
-  playerSelection = playerSelection.toLowerCase();
-  computerSelection = computerSelection.toLowerCase();
-  if (playerSelection == computerSelection) {
-    return `"Draw both selected same" ${playerSelection}`;
-  } else if (playerSelection == "rock" && computerSelection == "paper") {
-    return "You lose ! paper beats rock";
-  } else if (playerSelection == "paper" && computerSelection == "rock") {
-    return "You Win ! paper beats rock";
-  } else if (playerSelection == "scissors" && computerSelection == "rock") {
-    return "You lose ! rock beats scissors";
-  } else if (playerSelection == "rock" && computerSelection == "scissors") {
-    return "You Win ! rock beats scissors";
-  } else if (playerSelection == "paper" && computerSelection == "scissors") {
-    return "You lose ! scissors beats paper";
-  } else if (playerSelection == "scissors" && computerSelection == "paper") {
-    return "You Win ! scissors beats paper";
+  if (round < 5) {
+    round += 1;
+    round_div.textContent = round;
+
+    playerSelection = playerSelection.toLowerCase();
+    computerSelection = computerSelection.toLowerCase();
+    if (playerSelection == computerSelection) {
+      return `"Draw both selected same" ${playerSelection}`;
+    } else if (playerSelection == "rock" && computerSelection == "paper") {
+      updateCompScore();
+      return "You lose ! paper beats rock";
+    } else if (playerSelection == "paper" && computerSelection == "rock") {
+      updateHumanScore();
+      return "You Win ! paper beats rock";
+    } else if (playerSelection == "scissors" && computerSelection == "rock") {
+      updateCompScore();
+      return "You lose ! rock beats scissors";
+    } else if (playerSelection == "rock" && computerSelection == "scissors") {
+      updateHumanScore();
+      return "You Win ! rock beats scissors";
+    } else if (playerSelection == "paper" && computerSelection == "scissors") {
+      updateCompScore();
+      return "You lose ! scissors beats paper";
+    } else if (playerSelection == "scissors" && computerSelection == "paper") {
+      updateHumanScore();
+      return "You Win ! scissors beats paper";
+    } else {
+      return "some error !";
+    }
   } else {
-    return "some error !";
+    round = 0;
+    round_div.textContent = "5 round over";
+    if (humanScore > compScore) {
+      humanScore = 0;
+      compScore = 0;
+      return "You won";
+    } else if (humanScore < compScore) {
+      humanScore = 0;
+      compScore = 0;
+      return "You Lost Computer Wins";
+    } else if (humanScore == compScore) {
+      humanScore = 0;
+      compScore = 0;
+      return "Game Draw";
+    } else {
+      humanScore = 0;
+      compScore = 0;
+      return "Some Issue came !!";
+    }
   }
 }
 
-function game() {
-  for (let i = 0; i < 5; i++) {
-    let computer = getComputerChoice();
-    console.log(playRound(window.prompt(), computer));
-  }
+function updateCompScore() {
+  compScore++;
+  return (compScoreDiv.textContent = compScore);
 }
 
-game();
+function updateHumanScore() {
+  humanScore++;
+  return (humanScoreDiv.textContent = humanScore);
+}
+
+// function game() {
+//   for (let i = 0; i < 5; i++) {
+//     let computer = getComputerChoice();
+//     console.log(playRound(window.prompt(), computer));
+//   }
+// }
+
+// game();
